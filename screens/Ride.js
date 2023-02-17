f68fb07753ec738caaea094b9b3b66175f9ff88b
Maujr3456 <93012770+Maujr3456@users.noreply.github.com>
2023-02-17 18:47:21 -0300

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -73,25 +73,10 @@ export default class RideScreen extends Component {
           // Apenas para Android
           // No iOS utilize o Alert.alert
 
-          // show(
-          //   "Você alugou a bicicleta pela próxima 1 hora. Aproveite seu passeio!!",
-          //   ToastAndroid.SHORT
-          // );
-
-          // ToastAndroid.show(
-          //   "Você alugou a bicicleta pela próxima 1 hora. Aproveite seu passeio!!",
-          //   ToastAndroid
-          // );
-
-          // ToastAndroid.show(
-          //   "Você alugou a bicicleta pela próxima 1 hora. Aproveite seu passeio!!",
-          //   SHORT
-          // );
-
-          // ToastAndroid.show(
-          //   "Você alugou a bicicleta pela próxima 1 hora. Aproveite seu passeio!!",
-          //   ToastAndroid.SHORT
-          // );
+          ToastAndroid.show(
+            "Você alugou a bicicleta pela próxima 1 hora. Aproveite seu passeio!!",
+            ToastAndroid.SHORT
+          );
 
          // Alert.alert(
          //   "Você alugou a bicicleta pela próxima 1 hora. Aproveite seu passeio!!"
@@ -232,7 +217,7 @@ export default class RideScreen extends Component {
               //onChangeText={() => this.setState({ userId: text })}
               //onChangeText={this.setState({ userId: text })}
               //onChangeText={text => this.setState({ userId: text })}
-              //onChangeText={(text) => this.setState({ userId: text })}
+              onChangeText={(text) => this.setState({ userId: text })}
 
               placeholder={"Id do Usuário"}
               placeholderTextColor={"#FFFFFF"}
